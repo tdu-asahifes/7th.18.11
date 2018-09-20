ddf2159f91746f06d7ed624c0e51e7c5e4320359
Nicky0925 <aiueo0925aiueo@gmail.com>
2018-09-20 17:28:06 +0900

    (OrbitControls + three.min + main).js add

---
+++ js/main.js
@@ -0,0 +1,67 @@
+(function() {
+  'use strict';
+
+  var scene;
+  var light;
+  var ambient;
+  var camera;
+  var renderer;
+  var width = 1000;
+  var height = 500;
+  var controls;
+
+  var count = 100;
+  var i;
+  var size;
+  var box;
+
+  // scene ステージ
+  scene = new THREE.Scene();
+
+  // light
+  light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(0, 100, 30);
+  scene.add(light);
+  ambient = new THREE.AmbientLight(0x404040);
+  scene.add(ambient);
+
+  // camera 
+  camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+  camera.position.set(200, 100, 300);
+  camera.lookAt(scene.position);
+
+  // controls
+  controls = new THREE.OrbitControls(camera);
+  controls.autoRotate = true;
+
+  // renderer
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(width, height);
+  renderer.setClearColor(0xefefef);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  document.getElementById('stage').appendChild(renderer.domElement);
+
+  // picking
+  for (i = 0; i < count; i++) {
+    size = Math.random() * 20 + 10;
+    box = new THREE.Mesh(
+        new THREE.BoxGeometry(size, size, size),
+        new THREE.MeshPhongMaterial({color: Math.random() * 0xffffff})
+    );
+    box.position.set(
+        Math.random() * 200 - 100,
+        Math.random() * 200 - 100,
+        Math.random() * 200 - 100
+    );
+    scene.add(box);
+  }
+
+  function render() {
+    requestAnimationFrame(render);
+
+    controls.update();
+    renderer.render(scene, camera);
+  }
+  render();
+
+})();
